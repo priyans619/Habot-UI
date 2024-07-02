2c6481ebe9d0b6ada62f97a6bab996d2d6a2eb28
priyans619 <priyansh.2b@gmail.com>
2024-07-02 23:38:22 +0530

    add logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header className="bg-blue-500 text-white p-4">
-      <h1 className="text-2xl"></h1>
+    <header className=" text-black p-4 flex items-center justify-between">
+      <div className="flex items-center space-x-4">
+      <img 
+          src="/src/assets/logo.png" 
+          alt="Logo" 
+          className="w-[181px] h-[43px] sm:w-[181px] sm:h-[43px] md:w-[181px] md:h-[43px] lg:w-[181px] lg:h-[43px]"
+        />
+      </div>
+
     </header>
   );
 };
