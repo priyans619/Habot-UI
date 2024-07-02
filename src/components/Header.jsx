80a7fd201a25819a940b2265469eeffbb57e36bd
priyans619 <priyansh.2b@gmail.com>
2024-07-02 23:39:18 +0530

    nvigation links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,6 +11,13 @@ const Header = () => {
         />
       </div>
 
+      {/* Navigation Links */}
+      <div className="flex items-center space-x-4">
+        <a href="#section1" className="text-black hover:text-gray-300">Find Suppliers</a>
+        <a href="#section2" className="text-black hover:text-gray-300">Find Service Tags</a>
+        <a href="#login" class="text-[#00732F] border border-[#00732F] font-inter font-bold text-lg px-4 py-2 rounded ">Login/Signup</a>
+
+      </div>
     </header>
   );
 };
