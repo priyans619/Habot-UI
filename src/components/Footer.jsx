4d20afe97523d30b4b936363901d7a2895077044
priyans619 <priyansh.2b@gmail.com>
2024-07-02 23:40:30 +0530

    add footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-blue-500 text-white p-4">
+      <p>© 2024 My Website</p>
+    </footer>
+  );
+};
+
+export default Footer;
