a9df0444e5a38e87e4db4859683dac908216689b
priyans619 <priyansh.2b@gmail.com>
2024-07-03 15:34:13 +0530

    Render

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import Section1 from '../components/sections/Section1';
 
 const HomePage = () => {
   return (
     <div>
       <Header />
+      <main>
+        <Section1 />
+        
+      </main>
       <Footer />
     </div>
   );
